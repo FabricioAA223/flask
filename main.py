f350cf04859663771984cacdd948e467f73f31e6
Fabricio Alvarado <91855783+FabricioAA223@users.noreply.github.com>
2023-12-01 15:19:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import cv2
 import time
 import base64
 from datetime import datetime
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import re
+import os
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)  # Utiliza el índice 0 para la cámara integrada de la laptop
+
 def capture_images(interval, duration):
+    camera = cv2.VideoCapture('rtsp://192.168.0.100:554/11')  # Nueva conexión a la cámara
     start_time = time.time()
     images = []
     while time.time() - start_time < duration:
@@ -17,7 +22,45 @@ def capture_images(interval, duration):
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             images.append({'image': image_base64, 'timestamp': timestamp})
             time.sleep(interval)
+
+    camera.release()  # Cierra la conexión a la cámara al finalizar la captura
     return images
+
+def create_folder(drive, folder_name):
+    folder = drive.CreateFile({'title': folder_name, 'mimeType': 'application/vnd.google-apps.folder'})
+    folder.Upload()
+    return folder
+
+def upload_to_drive(images):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    # Crear una carpeta con el timestamp actual
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    folder_name = f"dataset_{timestamp}"
+    folder = create_folder(drive, folder_name)
+
+    for idx, image in enumerate(images):
+        image_data = base64.b64decode(image['image'])
+        
+        # Reemplazar caracteres no permitidos en nombres de archivos de Windows
+        timestamp = re.sub(r'[:]', '-', image['timestamp'])
+        file_name = f"image_{idx}_{timestamp}.jpg"
+
+        with open(file_name, 'wb') as file:
+            file.write(image_data)
+
+        # Subir la imagen a la carpeta en Google Drive
+        file_drive = drive.CreateFile({'title': file_name, 'parents': [{'id': folder['id']}]})
+        file_drive.SetContentFile(file_name)
+        file_drive.Upload()
+
+        file_drive.content.close()
+
+        os.remove(file_name)
+
+    print(f"Images uploaded to Google Drive folder: {folder_name}")
 
 @app.route('/')
 def index():
@@ -26,7 +69,8 @@ def index():
 @app.route('/get_images/<int:interval>/<int:duration>', methods=['GET'])
 def get_images(interval, duration):
     images = capture_images(interval, duration)
+    upload_to_drive(images)
     return jsonify({'images': images})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
